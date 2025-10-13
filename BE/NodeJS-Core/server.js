3642ced7e27ab9181c0174ff3ae2410f97b1152a
DuyNH <duyyl8a22016@gmail.com>
2025-10-13 12:03:06 +0700

    connect db success

--- BE/NodeJS-Core/server.js
+++ BE/NodeJS-Core/server.js
@@ -1,7 +1,28 @@
-const app = require("./index");
+// const app = require("./index");
 
-const port = 3000;
+// const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Running on http://localhost:${port}`);
+// app.listen(port, () => {
+//     console.log(`Running on http://localhost:${port}`);
+// });
+const { Sequelize } = require("sequelize");
+
+// Khởi tạo kết nối
+const sequelize = new Sequelize("blogdb", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
 });
+
+// Hàm kiểm tra
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Kết nối database thành công!");
+  } catch (error) {
+    console.error("❌ Kết nối thất bại:", error);
+  } finally {
+    await sequelize.close();
+  }
+}
+
+testConnection();
