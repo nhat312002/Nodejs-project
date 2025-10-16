8336249d59c90eefcdda3ad5637d2a175220fbc9
DuyNH <duyyl8a22016@gmail.com>
2025-10-16 12:49:26 +0700

    fix

--- BE/NodeJS-Core/server.js
+++ BE/NodeJS-Core/server.js
@@ -1,17 +1,14 @@
 const app = require("./index");
 
 const { Sequelize } = require("sequelize");
-<<<<<<< HEAD
 const { database } = require("./configs");
 // Khởi tạo kết nối
 // const sequelize = new Sequelize("blogdb", "root", "", {
 //   host: "localhost",
 //   dialect: "mysql",
 // });
-=======
 
 const { db } = require("models");
->>>>>>> 1a81b0ab33b75d5980574e6faf051cf8e8146471
 
 const port = 3000;
 
