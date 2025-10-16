3ec6b46c4cf9255ead3b8c267ab9e898248e18ec
nhatvq312002 <vqnhat312002@gmail.com>
2025-10-16 09:57:41 +0700

    update

---
+++ BE/NodeJS-Core/modules/languages/controllers/languageControllers.js
@@ -0,0 +1,63 @@
+const languageService = require("modules/language/services/languageService.js");
+const responseUtils = require("utils/responseUtils");
+
+const languageController = {
+    getAllLanguages: async (req, res) => {
+        try {
+            const languages = await languageService.getAllLanguages();
+            responseUtils.ok(res, languages);
+        } catch (error) {
+            responseUtils.error(res, error.message);
+        }
+    },
+    getLanguageById: async (req, res) => {
+        try {
+            const languageId = req.params.languageId;
+            const language = await languageService.getLanguageById(languageId);
+            if (!language) {
+                return responseUtils.notFound(res);
+            }
+            responseUtils.ok(res, language);
+        } catch (error) {
+            responseUtils.error(res, error.message);
+        }
+    },
+    createLanguage: async (req, res) => {
+        try {
+            const data = req.body;
+            const newLanguage = await languageService.createLanguage(data);
+            res.status(201).json(newLanguage);
+        } catch (error) {
+            responseUtils.error(res, error.message);
+        }
+    },
+    updateLanguage: async (req, res) => {
+        try {
+            const languageId = req.params.languageId;
+            const data = req.body;
+            const updatedLanguage = await languageService.updateLanguage(languageId, data);
+            responseUtils.ok(res, updatedLanguage);
+        } catch (error) {
+            if (error.message === "Language not found") {
+                return responseUtils.notFound(res);
+            }
+            responseUtils.error(res, error.message);
+        }
+    },
+    toggleLanguageStatus: async (req, res) => {
+        try {
+            const { languageId } = req.params;
+            const updatedLanguage = await languageService.toggleLanguageStatus(languageId);
+            if (!updatedLanguage) return responseUtils.notFound(res, "Language not found");
+
+            responseUtils.ok(res, {
+                message: `Language status updated to ${updatedLanguage.status}`,
+                data: updatedLanguage,
+            });
+        } catch (error) {
+            responseUtils.error(res, error.message);
+        }
+    },
+}
+
+module.exports = languageController;
---
+++ BE/NodeJS-Core/modules/languages/validations/languageValidations.js
@@ -0,0 +1,24 @@
+const Joi = require("joi");
+const { createLanguage, updateLanguage } = require("../services/languageService");
+const { createLanguage } = require("../controllers/languageController");
+
+const languageValidation = {
+    createLanguage: (data) => {
+        const schema = Joi.object({
+            code: Joi.string().min(2).max(5).required(),
+            name: Joi.string().min(3).max(255).required(),
+            status: Joi.string().valid("active", "disabled").required(),
+        });
+        return schema.validate(data);
+    },
+    updateLanguage: (data) => {
+        const schema = Joi.object({
+            code: Joi.string().min(2).max(5),
+            name: Joi.string().min(3).max(255),
+            status: Joi.string().valid("active", "disabled"),
+        });
+        return schema.validate(data);
+    },
+}
+
+module.exports = languageValidation;
